6d312a79976d07d7910460a15c517c5751fe97d0
maxwen <max.weninger@gmail.com>
2013-12-01 16:47:13 +0100

    [1/3] base: allow disabling non intrusive incall ui
    Change-Id: Ibe9c65f1fe024a859d98d96fd86199607a1060de

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2555,6 +2555,13 @@ public final class Settings {
         public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
 
         /**
+         * Should the non-intrsive incall ui be used
+         *
+         * @hide
+         */
+        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
